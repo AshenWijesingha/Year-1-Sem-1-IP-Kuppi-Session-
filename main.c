7a4c4ba7bea12467f5fbe66bfcec2cd6516c8d35
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2021-10-29 12:34:52 +0000

    Changed

--- main.c
+++ main.c
@@ -7,16 +7,57 @@ int main(void) {
   char letter;
   double number;
 
+//inputs
+/*
+  Integer
+    %d
+    %i
   
+  floating-point
+    %f
+    %lf
+
+  Character
+    %c
+
+  String
+    %s
+*/
+
   scanf("%d",&quentity);
   scanf("%f",&price);
   scanf(" %c",&letter);
   scanf("%lf",&number);
 
+
+//Outputs
+/*
+  Integer
+    %d
+    %i
+  
+  floating-point
+    %f
+    %lf
+
+  Character
+    %c
+
+  String
+    %s
+*/
+
   printf("%d",quentity);
   printf("%lf",price);
   printf(" %c",letter);
   printf("%lf",number);
+
+  printf("%d\n", 455);
+  printf("%d\n", -455);
+  printf("%i\n", 455);
+  printf("%f\n", 1234567.89);
+  printf("%.2f\n", 3.446);
+  printf(" %c\n",'A');
 
   return 0;
 }
