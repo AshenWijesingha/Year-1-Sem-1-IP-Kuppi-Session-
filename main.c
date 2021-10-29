22925b0a9b3e6c0cf45a3b059de06a877e68907c
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2021-10-29 12:29:43 +0000

    Inputs and Outputs

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World..!\n");
+
+  int quentity;
+  float price;
+  char letter;
+  double number;
+
+  
+  scanf("%d",&quentity);
+  scanf("%f",&price);
+  scanf(" %c",&letter);
+  scanf("%lf",&number);
+
+  printf("%d",quentity);
+  printf("%lf",price);
+  printf(" %c",letter);
+  printf("%lf",number);
+
   return 0;
 }
